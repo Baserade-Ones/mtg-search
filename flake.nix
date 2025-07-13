8b82397405044f76483d9e8fbeae71977f357f70
axel paulander <axel.paulander@gmail.com>
2025-07-13 13:02:56 +0200

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,80 @@
+{
+  description = "Basic rust env flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    crane.url = "github:ipetkov/crane";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    crane,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {inherit system overlays;};
+        inherit (pkgs) lib;
+
+        rustToolchain = pkgs.rust-bin.stable."1.88.0".default;
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+        src = lib.cleanSourceWith {
+          src = ./.; # The original, unfiltered source
+          filter = path: type:
+            (lib.hasInfix "assets/" path)
+            || (lib.hasInfix "mana/" path)
+            ||
+            # Default filter from crane (allow .rs files)
+            (craneLib.filterCargoSources path type);
+        };
+
+        nativeBuildInputs = with pkgs; [rustToolchain pkg-config];
+        buildInputs = with pkgs; [
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXrandr
+          xorg.libXi
+          xorg.libxcb
+          libxkbcommon
+          vulkan-loader
+          wayland
+          libGL
+        ];
+
+        # because we'll use it for both `cargoArtifacts` and `bin`
+        commonArgs = {
+          inherit src buildInputs nativeBuildInputs;
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        bin = craneLib.buildPackage (commonArgs
+          // {
+            inherit cargoArtifacts;
+          });
+      in {
+        packages = {
+          inherit bin;
+          default = bin;
+        };
+        devShells.default = pkgs.mkShell {
+          # instead of passing `buildInputs` / `nativeBuildInputs`,
+          # we refer to an existing derivation here
+          inputsFrom = [bin];
+          shellHook = ''
+            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath buildInputs)}";
+          '';
+        };
+      }
+    );
+}
